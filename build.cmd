fdb9673ab3be0c0f0d84fc28c066f758a6093426
Alexey Sosnin <sosnin.alexey@outlook.com>
2015-05-08 20:51:28 +0300

    add missing files

--- build.cmd
+++ build.cmd
@@ -7,11 +7,14 @@ mkdir src\Microsoft.Rtc.Collaboration
 echo .
 echo Building LyncSDK Desktop
 .\apps\AsyncCodeGenerator\x86\AsyncCodeGenerator.exe .\libs\LyncSDK\Desktop\Microsoft.Lync.Model.dll /out:.\src\LyncSDK\Desktop\Microsoft.Lync.Model.AsyncExtensions.cs.pp /ns:$rootnamespace$
+.\apps\AsyncCodeGenerator\x86\AsyncCodeGenerator.exe .\libs\LyncSDK\Desktop\Microsoft.Lync.Model.dll /out:.\src\LyncSDK\Desktop\Microsoft.Lync.Model.AsyncExtensions.cs /ns:Microsoft.Lync.Model.AsyncExtensions
 
 echo .
 echo Building LyncSDK Silverlight
 .\apps\AsyncCodeGenerator\x86\AsyncCodeGenerator.exe .\libs\LyncSDK\Silverlight\Microsoft.Lync.Model.dll /out:.\src\LyncSDK\Silverlight\Microsoft.Lync.Model.AsyncExtensions.cs.pp /ns:$rootnamespace$
+.\apps\AsyncCodeGenerator\x86\AsyncCodeGenerator.exe .\libs\LyncSDK\Silverlight\Microsoft.Lync.Model.dll /out:.\src\LyncSDK\Silverlight\Microsoft.Lync.Model.AsyncExtensions.cs /ns:Microsoft.Lync.Model.AsyncExtensions
 
 echo .
 echo Building Microsoft.Rtc.Collaboration
 .\apps\AsyncCodeGenerator\x64\AsyncCodeGenerator.exe .\libs\Microsoft.Rtc.Collaboration\Microsoft.Rtc.Collaboration.dll /out:.\src\Microsoft.Rtc.Collaboration\Microsoft.Rtc.Collaboration.AsyncExtensions.cs.pp /ns:$rootnamespace$
+.\apps\AsyncCodeGenerator\x64\AsyncCodeGenerator.exe .\libs\Microsoft.Rtc.Collaboration\Microsoft.Rtc.Collaboration.dll /out:.\src\Microsoft.Rtc.Collaboration\Microsoft.Rtc.Collaboration.AsyncExtensions.cs /ns:Microsoft.Rtc.Collaboration.AsyncExtensions
--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,6 @@
 @echo off
 
+.\apps\NuGet\NuGet.exe restore .\src\dlls\dlls.sln
+
 set msbuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 %msbuild% .\src\dlls\dlls.sln /t:Rebuild /p:Configuration=Release
