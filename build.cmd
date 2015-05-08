8dbef578ff6665e94231108e33ea2b20defce2f9
Alexey Sosnin <sosnin.alexey@gmail.com>
2015-05-08 14:47:27 +0300

    Microsoft.Rtc.Collaboration package

--- build.cmd
+++ build.cmd
@@ -1,5 +1,17 @@
 @echo off
 
+mkdir src\LyncSDK\Desktop
+mkdir src\LyncSDK\Silverlight
+mkdir src\Microsoft.Rtc.Collaboration
+
+echo .
+echo Building LyncSDK Desktop
 .\apps\AsyncCodeGenerator\x86\AsyncCodeGenerator.exe .\libs\LyncSDK\Desktop\Microsoft.Lync.Model.dll /out:.\src\LyncSDK\Desktop\Microsoft.Lync.Model.AsyncExtensions.cs.pp /ns:$rootnamespace$
+
+echo .
+echo Building LyncSDK Silverlight
 .\apps\AsyncCodeGenerator\x86\AsyncCodeGenerator.exe .\libs\LyncSDK\Silverlight\Microsoft.Lync.Model.dll /out:.\src\LyncSDK\Silverlight\Microsoft.Lync.Model.AsyncExtensions.cs.pp /ns:$rootnamespace$
+
+echo .
+echo Building Microsoft.Rtc.Collaboration
 .\apps\AsyncCodeGenerator\x64\AsyncCodeGenerator.exe .\libs\Microsoft.Rtc.Collaboration\Microsoft.Rtc.Collaboration.dll /out:.\src\Microsoft.Rtc.Collaboration\Microsoft.Rtc.Collaboration.AsyncExtensions.cs.pp /ns:$rootnamespace$
--- package.cmd
+++ package.cmd
@@ -1,2 +1,19 @@
 @echo off
+
+mkdir nuget\packages
+
+echo .
+echo Packaging LyncSDK.AsyncExtensions
 .\apps\NuGet\NuGet.exe pack nuget\LyncSDK\LyncSDK.AsyncExtensions.nuspec -OutputDirectory .\nuget\packages\
+
+echo .
+echo Packaging LyncSDK.AsyncExtensions.Sources
+.\apps\NuGet\NuGet.exe pack nuget\LyncSDK.AsyncExtensions.Sources\LyncSDK.AsyncExtensions.Sources.nuspec -OutputDirectory .\nuget\packages\
+
+echo .
+echo Packaging LyncSDK.AsyncExtensions
+.\apps\NuGet\NuGet.exe pack nuget\Microsoft.Rtc.Collaboration.AsyncExtensions\Microsoft.Rtc.Collaboration.AsyncExtensions.nuspec -OutputDirectory .\nuget\packages\
+
+echo .
+echo Packaging LyncSDK.AsyncExtensions.Sources
+.\apps\NuGet\NuGet.exe pack nuget\Microsoft.Rtc.Collaboration.AsyncExtensions.Sources\Microsoft.Rtc.Collaboration.AsyncExtensions.Sources.nuspec -OutputDirectory .\nuget\packages\
